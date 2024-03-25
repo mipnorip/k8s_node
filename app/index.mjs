15506710769206be0b893b4fcea4153efe055b9a
Ilya Maslov <muser@Ilyas-MacBook-Air.local>
2024-03-25 20:14:54 +0300

    create simple application on node.js

---
+++ app/index.mjs
@@ -0,0 +1,16 @@
+import express from 'express';
+import os from 'os';
+
+const app = express();
+const PORT = 3000;
+
+
+app.get('/', (req, res) => {
+    const message = `hello from ${os.hostname()}`;
+    console.log(req.ip, message);
+    res.send(message);
+});
+
+app.listen(PORT, () => {
+    console.log(`server start at port ${PORT}`);
+});
