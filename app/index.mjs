f742e16322b3041f632575cd9e8ed2d5dbfa5365
Ilya M <i@ilyamaslov.ru>
2024-03-25 22:32:39 +0300

    add path /time to app

--- app/index.mjs
+++ app/index.mjs
@@ -1,15 +1,45 @@
 import express from 'express';
 import os from 'os';
+import { Client } from 'pg'
+import process from 'process';
 
 const app = express();
 const PORT = 3000;
-
+const PG_HOST = process.env.POSTGRESS_HOST;
+const PG_USER = process.env.POSTGRESS_HOST;
+const PG_PASSWORD = process.env.POSTGRESS_HOST;
+const PG_DB = process.env.POSTGRESS_DB;
 
 app.get('/', (req, res) => {
     const message = `v1.0 hello from ${os.hostname()}`;
     console.log(req.ip, message);
     res.send(message);
 });
+
+app.get('/time', (req, res) => {
+    console.log(req.ip, message);
+    const client = new Client({
+        host: PG_HOST,
+        database: PG_DB,
+        user: PG_USER,
+        password: PG_PASSWORD,
+        port: 5432
+    });
+    client.connect().then(() => {
+        client.query('NOW();', (err, res) => {
+            if (err){
+                console.log(`error: ${err}`);
+                const time = err;
+                return;
+            } else {
+                const time = res;
+            }
+            res.send(time)
+            return;
+        })
+    })
+    res.send('Error. Not connected to DB')
+})
 
 app.listen(PORT, () => {
     console.log(`server start at port ${PORT}`);
